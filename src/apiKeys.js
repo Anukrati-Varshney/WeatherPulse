9825a4e1bcd389b59e5c093810d810be6f80a44b
Anukrati-Varshney <anukrativarshney9@gmail.com>
2023-09-28 01:20:28 +0530

    my first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ef860e3a9166384b9a3f1b13d63bf342",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
