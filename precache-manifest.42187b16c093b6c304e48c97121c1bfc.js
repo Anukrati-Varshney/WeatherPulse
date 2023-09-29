07efb6b9388a57ba4fc93653060542547eed9795
Anukrati-Varshney <anukrativarshney9@gmail.com>
2023-09-29 22:12:24 +0530

    Updates

---
+++ precache-manifest.42187b16c093b6c304e48c97121c1bfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9da6bc29a990a7928ecc08cef69dc7e",
+    "url": "/WeatherPulse/index.html"
+  },
+  {
+    "revision": "0b7a585dbc9130fdd8b7",
+    "url": "/WeatherPulse/static/css/main.fbc16618.chunk.css"
+  },
+  {
+    "revision": "4ade54d7913edb68c0d6",
+    "url": "/WeatherPulse/static/js/2.75e5e61b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherPulse/static/js/2.75e5e61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7a585dbc9130fdd8b7",
+    "url": "/WeatherPulse/static/js/main.313a732c.chunk.js"
+  },
+  {
+    "revision": "72caa7651089382a7cf2",
+    "url": "/WeatherPulse/static/js/runtime-main.fdaaab07.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/WeatherPulse/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/WeatherPulse/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/WeatherPulse/static/media/city.e1698332.jpg"
+  }
+]);
